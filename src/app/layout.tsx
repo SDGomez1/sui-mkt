dbcdd9d56892cbc2936b3d183330b3bed6bbd045
Sergio <sergio.gomez2000@hotmail.com>
2025-05-08 10:16:39 -0500

    set up clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,6 +58,18 @@ export default function RootLayout({
             <!-- End Meta Pixel Code -->`,
           }}
         />
+        <Script
+          id="clarityPixelInit"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "rgeothn41v");                        `,
+          }}
+        />
         <noscript>
           <img
             height="1"
