2af861fe8310636f57596b8b75b64d7a7e338a3e
Sergio Gomez <sergio.gomez2000@hotmail.com>
2025-05-07 20:35:00 -0500

    features in mama landing

--- src/app/landing/mama/ProductGallery.tsx
+++ src/app/landing/mama/ProductGallery.tsx
@@ -6,18 +6,39 @@ import { useState } from "react";
 
 export default function ProductGallery() {
     const imageArray = [heroImage1, heroImage2, heroImage1, heroImage2]
+    const [isVideo, setIsVideo] = useState(true);
     const [currentImage, setCurrentImage] = useState(imageArray[0])
-        
+
     return (
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-8 ">
             <div>
-                <Image src={currentImage} alt="" className="aspect-video w-auto h-60"></Image>
+                {isVideo ? <video className="bg-green-500 aspect-video h-60 w-auto"></video> : <Image src={currentImage} alt="" className="aspect-video w-auto h-60"></Image>}
+
             </div>
-            <div className ="flex gap-4">
-                <Image src={imageArray[0]} alt="" className="aspect-square w-auto h-20"/>
-                <Image src={imageArray[1]} alt="" className="aspect-square w-auto h-20"/>
-                <Image src={imageArray[2]} alt="" className="aspect-square w-auto h-20"/>
-                <Image src={imageArray[3]} alt="" className="aspect-square w-auto h-20"/>
+            <div className="flex gap-4">
+                <Image src={imageArray[0]} alt="" className="aspect-square w-auto h-20"
+                    onClick={() => {
+                        setIsVideo(true)
+                        setCurrentImage(imageArray[0])
+                    }} />
+                <Image src={imageArray[1]} alt="" className="aspect-square w-auto h-20"
+                    onClick={() => {
+                        setIsVideo(false)
+                        setCurrentImage(imageArray[1])
+                    }}
+                />
+                <Image src={imageArray[2]} alt="" className="aspect-square w-auto h-20"
+                    onClick={() => {
+                        setIsVideo(false)
+                        setCurrentImage(imageArray[2])
+                    }}
+                />
+                <Image src={imageArray[3]} alt="" className="aspect-square w-auto h-20"
+                    onClick={() => {
+                        setIsVideo(false)
+                        setCurrentImage(imageArray[3])
+                    }}
+                />
             </div>
         </div>
     );
--- src/app/landing/mama/page.tsx
+++ src/app/landing/mama/page.tsx
@@ -4,9 +4,38 @@ import ProductGallery from "./ProductGallery";
 export default function Page() {
   return (
     <section className="overflow-x-hidden max-w-svw">
-      <div className="flex px-4 xl:px-40 flex-col xl:flex-row">
+      <div className="flex px-4 xl:px-40 flex-col xl:flex-row justify-between py-8">
         <ProductGallery />
+        <div className="lg: w-1/2">
+          <h1 className="font-old-standard text-3xl text-primary">
+            ¿HACE CUÁNTO NO LE DICES A MAMÁ LO QUE SIGNIFICA PARA TI?
+          </h1>
+          <p className="font-bold text-2xl mt-4 text-primary">$39.900</p>
+          <p className="font-bold text-xl mt-4 ">
+            Este no es un regalo más. Es una herramienta para conectar.
+          </p>
+          <p className="text-xl mt-4">
+            Cuando no sabes cómo decirlo, esta vela lo dice por ti. A veces,
+            queremos decir tanto… pero las palabras no alcanzan. Este kit nació
+            de esa necesidad:{" "}
+            <span className=" underline">
+              de decir lo importante sin hablar demasiado.
+            </span>
+          </p>
+
+          <p className="text-xl mt-4">Con este kit lograras:</p>
+          <ul className="text-xl">
+            <li>✅ Que el regalo no solo guste, sino que toque el alma. </li>
+            <li>✅ Que tu mensaje se sienta, no solo se lea.</li>
+            <li>
+              ✅ Que quien lo reciba te recuerde por cómo la hiciste sentir.
+            </li>
+          </ul>
+          <p className="text-xl mt-4 font-bold">¿Quieres dar algo que no se olvide?</p>
+          <p className=" mt-4 font-bold">Empieza por aquí.</p>
+        </div>
       </div>
+
       <Footer />
     </section>
   );
