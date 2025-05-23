93b9d0287537ce0032dcdf448245e0258b337513
Sergio <sergio.gomez2000@hotmail.com>
2025-05-22 19:01:51 -0500

    fix: state on mobile for google login

--- src/components/auth/GoogleButton.tsx
+++ src/components/auth/GoogleButton.tsx
@@ -3,10 +3,13 @@ import { GoogleIcon } from "@/assets/icons/GoogleIcon";
 import { LoadingSpinner } from "@/assets/icons/LoadingSpinner";
 import { authClient } from "@/lib/auth-client";
 import { cn } from "@/lib/utils";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function GoogleButton({ className }: { className?: string }) {
   const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
   return (
     <button
       className={cn(
