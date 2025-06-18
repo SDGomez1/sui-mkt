f1bf71e26e8a9b6807956924ae411ea4d75b5bfd
Sergio Gomez <sergio.gomez2000@hotmail.com>
2025-06-17 19:58:21 -0500

    fix: page naming for compilation

--- src/app/linktree/page.tsx
+++ src/app/linktree/page.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 import TextLogo from "@/assets/icons/TextLogo";
 import { CircleArrowRight } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-export default function page() {
-    const router = useRouter()
+export default function Page() {
+  const router = useRouter();
   return (
     <div className="min-h-screen w-full flex justify-center items-center flex-col text-center px-6">
       <TextLogo />
@@ -16,10 +16,24 @@ export default function page() {
         Transforma tu lugar secreto
       </h3>
 
-      <button className="bg-primary text-white w-full rounded-2xl mt-6 mb-4 py-2 flex items-center justify-center gap-4" onClick={() => router.push("/#products") }>Catálogo <CircleArrowRight className="text-white"/></button>
-      <button className="hidden bg-primary text-white w-full rounded-2xl mt-6 mb-4 py-2 items-center justify-center gap-4">Ebooks <CircleArrowRight className="text-white"/></button>
-      <button className="bg-primary text-white w-full rounded-2xl mt-6 mb-4 py-2 flex items-center justify-center gap-4" onClick={() => router.push("https://wa.me/573173914524")}>WhatsApp <CircleArrowRight className="text-white"/></button>
-      <Link href={"/"} className="underline mt-10">suivelas.com</Link>
+      <button
+        className="bg-primary text-white w-full rounded-2xl mt-6 mb-4 py-2 flex items-center justify-center gap-4"
+        onClick={() => router.push("/#products")}
+      >
+        Catálogo <CircleArrowRight className="text-white" />
+      </button>
+      <button className="hidden bg-primary text-white w-full rounded-2xl mt-6 mb-4 py-2 items-center justify-center gap-4">
+        Ebooks <CircleArrowRight className="text-white" />
+      </button>
+      <button
+        className="bg-primary text-white w-full rounded-2xl mt-6 mb-4 py-2 flex items-center justify-center gap-4"
+        onClick={() => router.push("https://wa.me/573173914524")}
+      >
+        WhatsApp <CircleArrowRight className="text-white" />
+      </button>
+      <Link href={"/"} className="underline mt-10">
+        suivelas.com
+      </Link>
     </div>
   );
 }
