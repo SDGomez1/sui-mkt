83aaf6c5ed3a94a057b751acb7a4ce30e668a9ee
Sergio <sergio.gomez2000@hotmail.com>
2025-07-03 12:45:44 -0500

    feat: added playlist url

---
+++ src/app/playlist/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function page() {
+    redirect("https://www.youtube.com/watch?v=IHm6KzALMTE&list=PLAg_BPYDOmrR0rS_8knCg-5tOiM_KFQ8v")
+}
