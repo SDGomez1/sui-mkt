12af02798bd5d513a4170cbec69995e56b957cec
Sergio Gomez <sergio.gomez2000@hotmail.com>
2026-03-10 12:33:02 -0500

    feat: added meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,25 @@ export default function RootLayout({
         className={`${questrial.className} ${oldStandard.variable} ${openSans.variable} antialiased ${poppins.className}`}
       >
         <Script
+          id="metaPixelInit"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            <!-- Meta Pixel Code -->
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1409422410201442');
+            fbq('track', 'PageView');
+            <!-- End Meta Pixel Code -->`,
+          }}
+        />
+        <Script
           id="tiktok-pixel"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
